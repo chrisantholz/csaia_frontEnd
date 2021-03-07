e801afd5539eedb65c3b8e9c02781fca2067d51a
Ben Harvey <benjamin.c.harvey@ndsu.edu>
2021-03-07 13:54:52 -0600

    integrating flight uploading

--- csaia-angular/src/app/app.module.ts
+++ csaia-angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,7 +31,8 @@ import { AdminSettingsComponent } from './admin-settings/admin-settings.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
